b15dee9eaa99512395ba764e2f529da8cc04ab09
matty260 <mateuszhudzik.dev@gmail.com>
2024-10-12 20:12:48 +0200

    feat: init project and configure TypeScript with Vue integration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-export {};
+import { CompilerOptions, ScriptTarget, ModuleKind, ModuleResolutionKind } from 'typescript';
+
+const tsconfig: { compilerOptions: CompilerOptions; include: string[]; exclude: string[] } = {
+  compilerOptions: {
+    target: ScriptTarget.ESNext,      
+    module: ModuleKind.ESNext,
+    moduleResolution: ModuleResolutionKind.NodeJs,
+    lib: ["dom", "esnext"],
+    allowSyntheticDefaultImports: true, 
+    esModuleInterop: true,
+    strict: true, 
+    noImplicitAny: true,
+    skipLibCheck: true, 
+    forceConsistentCasingInFileNames: true, 
+    resolveJsonModule: true, 
+    isolatedModules: true,  
+    declaration: true,   
+    declarationDir: "./types", 
+    outDir: "./dist",  
+    baseUrl: ".", 
+    paths: {
+      "@/*": ["src/*"]
+    }
+  },
+  include: ["src/**/*", "shims-vue.d.ts"], 
+  exclude: ["node_modules", "dist", "**/*.spec.ts"]
+};
+
+export = tsconfig;
